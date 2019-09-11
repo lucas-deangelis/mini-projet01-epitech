6543b79edc8705872c7eab19744ef882c9f9d2db
Lucas De Angelis <lucas@localhost.localdomain>
2019-09-11 14:53:09 +0200

    Ajout de quelques utilisateurs test

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,29 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Gotham.Accounts.User
+alias Gotham.Times.{Clock, Workingtime}
+alias Gotham.Repo
+
+# On vide les tables de leur contenu pour repartir de 0 pour les tests
+# Le cascade permet de viser les deux autres tables qui référencent users 
+Repo.query("TRUNCATE TABLE users CASCADE", [])
+
+%User{
+    username: "joerogan",
+    email: "joe.rogan@mail.com"
+}
+|> Repo.insert!()
+
+%User{
+    username: "peterattia",
+    email: "peter.attia@mail.com"
+}
+|> Repo.insert!()
+
+%User{
+    username: "timferriss",
+    email: "tim.ferriss@mail.com"
+}
+|> Repo.insert!()
