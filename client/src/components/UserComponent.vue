5d0bb13ba61341279eaa245fee601d208df3e727
mdufour <mathieu.dufour@whereyoulove.com>
2019-09-11 13:10:56 +0200

    ajout de usercomponent + bootstrap pour axios

---
+++ client/src/components/UserComponent.vue
@@ -0,0 +1,44 @@
+<script>
+// This component defines an User.
+// The user data is fetched from the api.
+
+export default {
+    name: 'User',
+
+    props: [
+        'user'
+    ],
+
+    data() {
+        return {
+            id: '',
+            username: '',
+            email: ''
+        }
+    },
+
+    components: {
+
+    },
+
+    mounted() {
+        getUser: {
+            let url = window.apiUrl + '/api/users/' + this.userID;
+
+            window.axios.get(url)
+            .then(response => {
+                this.user = response.data;
+            })
+            .catch(error => {
+                print(error);
+            });
+        }
+
+    },
+
+    methods: {
+
+    }
+}
+
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+// Global components
+Vue.component(
+    'user-component',
+    require('./components/UserComponent.vue')
+);
+
+
 Vue.config.productionTip = false
 
-new Vue({
+// Instantiate the VUE and attach it to #app
+const app = new Vue({
   render: h => h(App),
 }).$mount('#app')
