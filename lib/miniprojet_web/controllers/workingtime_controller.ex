5f641bc289ead4636c18f9628f7de99e83c4ee6b
Lucas De Angelis <lucas@localhost.localdomain>
2019-09-11 16:07:54 +0200

    la route workingtime/:userID/:workingtimeID renvoie bien le workingtime associé à :workingtimeID et non plus :userID

--- lib/miniprojet_web/controllers/workingtime_controller.ex
+++ lib/miniprojet_web/controllers/workingtime_controller.ex
@@ -15,19 +15,21 @@ defmodule GothamWeb.WorkingtimeController do
     end
   end
 
-  def show(conn, %{"userID" => id}) do
-    workingtime = Times.get_workingtime!(id)
+  def show(conn, %{"userID" => userId, "workingtimeID" => workingtimeId}) do
+    workingtime = Times.get_workingtime!(workingtimeId)
     render(conn, "show.json", workingtime: workingtime)
   end
 
   def show_by_attr(conn, %{"userID" => id}) do
     params = conn.query_params
-
+    
+'''
     # check that there a start and a end params
     if Map.has_key?(params, "start") and Map.has_key?(params, "end") do
       workingtime = Times.get_workingtime_by_attr(id, Map.get(params, "start"), Map.get(params, "end")) # get the working time by attributes
       render(conn, "show.json", workingtime: workingtime)
     end
+'''
   end
 
   def update(conn, %{"id" => id, "workingtime" => workingtime_params}) do
