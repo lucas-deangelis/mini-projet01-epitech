c2c2517c4e547012166b45c3338e23ec25140f53
Lucas De Angelis <lucas@localhost.localdomain>
2019-09-12 09:57:16 +0200

    avancement projet

--- lib/miniprojet/times/workingtime.ex
+++ lib/miniprojet/times/workingtime.ex
@@ -13,8 +13,8 @@ defmodule Gotham.Times.Workingtime do
   @doc false
   def changeset(workingtime, attrs) do
     workingtime
-    |> cast(attrs, [:start, :end])
-    |> validate_required([:start, :end])
+    |> cast(attrs, [:start, :end, :user])
+    |> validate_required([:start, :end, :user])
     #|> validate_format(~N[:end], ~r/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/)
     #|> validate_format(~N[:start], ~r/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/)
   end
--- lib/miniprojet_web/controllers/workingtime_controller.ex
+++ lib/miniprojet_web/controllers/workingtime_controller.ex
@@ -6,7 +6,14 @@ defmodule GothamWeb.WorkingtimeController do
 
   action_fallback GothamWeb.FallbackController
 
-  def create(conn, %{"userID" => id, "workingtime" => workingtime_params}) do
+  def createUsrId(conn, %{"userID" => id, "workingtime" => workingtime_dates}) do
+
+    newEnd = NaiveDateTime.from_iso8601(workingtime_dates.end)
+    newStart = NaiveDateTime.from_iso8601(workingtime_dates.start)
+
+    workingtime_params = %Workingtime{start: newStart, end: newEnd, user: id}
+    IO.inspect(workingtime_dates)
+
     with {:ok, %Workingtime{} = workingtime} <- Times.create_workingtime(workingtime_params) do
       conn
       |> put_status(:created)
@@ -14,6 +21,19 @@ defmodule GothamWeb.WorkingtimeController do
       |> render("show.json", workingtime: workingtime)
     end
   end
+
+  # Not required but useful for testing purposes
+  def create(conn, %{"workingtime" => workingtime_params}) do
+    IO.inspect(workingtime_params)
+    with {:ok, %Workingtime{} = workingtime} <- Times.create_workingtime(workingtime_params) do
+      conn
+      |> put_status(:created)
+    # |> put_resp_header("location", Routes.workingtime_path(conn, :show, workingtime))
+    # |> render("show.json", workingtime: workingtime)
+    end
+  end
+
+  
 
   def show(conn, %{"userID" => userId, "workingtimeID" => workingtimeId}) do
     workingtime = Times.get_workingtime!(workingtimeId)
@@ -29,6 +49,8 @@ defmodule GothamWeb.WorkingtimeController do
       workingtime = Times.get_workingtime_by_attr(id, Map.get(params, "start"), Map.get(params, "end")) # get the working time by attributes
       render(conn, "show.json", workingtime: workingtime)
     end
+
+    Memo : pour faire le post :userID, il faut que le post marche. Pour savoir si le post marche, une route pourrait être pas mal mais peut être pas nécessaire (console serveur qu'a un feedback )
 '''
   end
 
