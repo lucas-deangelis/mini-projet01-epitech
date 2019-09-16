803099cf7cce9363e69cac63704c164539d73226
mdufour <mathieu.dufour@whereyoulove.com>
2019-09-16 16:21:44 +0200

    vue morris + first chart

--- client/src/store/modules/workingtime.js
+++ client/src/store/modules/workingtime.js
@@ -1,12 +1,16 @@
 // initial state
 const state = {
   workingTimes: [],
-  userId: 1
+  userId: 1,
+  received: false
 }
 
 // getters
 const getters = {
-
+  // parse state.workingTimes to adapt it for the line chart in chartmanagercomponent
+  getWK: state => {
+    return state.workingTimes
+  }
 }
 
 // actions
@@ -16,28 +20,30 @@ const actions = {
     const savedWorkingTimes = [...state.workingTimes]
     // empty working times
     commit('setWorkingTimes', [])
+    commit('setReceived', false)
 
     // get working times
-    let url = window.apiUrl + '/api/workingtimes/' + userId + '?start=' + '' + '&end=' + '';  //+ this.$root.user.id;
+    let url = window.apiUrl + '/api/workingtimes/' + userId + '?start=' + '' + '&end=' + ''  //+ this.$root.user.id;
 
     window.axios.get(url)
     .then(response => {
         // parse the data
-        let workingTimesDatas = JSON.parse(JSON.stringify(response.data.data));
-        let parsedWorkingTimes = [];
+        let workingTimesDatas = JSON.parse(JSON.stringify(response.data.data))
+        let parsedWorkingTimes = []
 
         for (const item of workingTimesDatas) {
             parsedWorkingTimes.push({
                 'id': item.id,
                 'start': item.start.replace('T', ' '),
                 'end': item.end.replace('T', ' ')
-            });
+            })
         }
         // assign the parsed datas to our state.workingtimes
         commit('setWorkingTimes', parsedWorkingTimes)
+        commit('setReceived', true)
     })
     .catch(error => {
-        console.error(error);
+        console.error(error)
     });
   },
 
@@ -53,7 +59,12 @@ const mutations = {
   // set the state.workingTimes
   setWorkingTimes (state, workingTimes) {
     state.workingTimes = workingTimes
+  },
+
+  setReceived (state, received) {
+    state.received = received
   }
+
 }
 
 export default {
