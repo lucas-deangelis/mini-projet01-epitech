118210249eac880d58d098074515ba39833275d7
Lucas De Angelis <lucas.de-angelis@epitech.eu>
2019-09-19 14:27:58 +0200

    docker-compose works and the app is accessible on port 8080

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 echo "docker starting"
 
 # Wait until Postgres is ready
-#while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
-#do
-#  echo "$(date) - waiting for database to start"
-#  sleep 2
-#done
+while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
 
 # Create, migrate, and seed database if it doesn't exist.
 if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
@@ -17,4 +17,4 @@ if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
   echo "Database $PGDATABASE created."
 fi
 
-exec mix phx.server
+exec mix phx.server&
