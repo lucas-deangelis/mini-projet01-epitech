efcf00fd829975107523e2a22ca3e2e0cdca3827
Lucas De Angelis <lucas.de-angelis@epitech.eu>
2019-09-24 11:10:48 +0200

    Error correction

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,8 @@ config :miniprojet, GothamWeb.Endpoint,
   url: [host: "gotham-timemanager.heroku.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"
-  
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
 config :miniprojet, Gotham.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
