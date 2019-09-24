1ca6a1ccff19b6ccd4cdfa00cb6231b115cd239c
Lucas De Angelis <lucas.de-angelis@epitech.eu>
2019-09-24 11:07:04 +0200

    Database connection to Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,13 +10,24 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :miniprojet, GothamWeb.Endpoint,
+  lod_from_system_env: true,
   url: [host: "gotham-timemanager.heroku.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  nager.herokuapp.com request_id=fb6bbee8-e171-464e-94cf-702b252e82dc fwd="163.5.10"
+
+config :miniprojet, Gotham.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Configure your database
 
 # ## SSL Support
 #
