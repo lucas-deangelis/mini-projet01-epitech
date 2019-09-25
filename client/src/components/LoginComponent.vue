8cd75bc92b210401231bff062cdf2aa038bf1b55
mdufour <mathieu.dufour@whereyoulove.com>
2019-09-25 17:33:54 +0200

    update package and change login register page

--- client/src/components/LoginComponent.vue
+++ client/src/components/LoginComponent.vue
@@ -56,9 +56,3 @@ export default {
 	}
 }
 </script>
-
-<style>
-@import url('../assets/fontawesome/css/all.css');
-@import url('../assets/css/style.css');
-@import url('../assets/css/transition.css');
-</style>
