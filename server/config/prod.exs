d7ac7b2aaa0beb9e0cc67ce3c27000a2211752d4
mdufour <mathieu.dufour@whereyoulove.com>
2019-09-27 17:46:41 +0200

    heroku yml to run mix ecto create + heroku deployer for travis

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :miniprojet, GothamWeb.Endpoint,
   lod_from_system_env: true,
-  url: [scheme: "https", host: "gotham-timemanager", port: 443],
+  url: [scheme: "https", host: "gotham-time-manager", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
