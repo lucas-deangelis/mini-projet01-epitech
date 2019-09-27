b2c08a69ad856d6c0c37d1c8478b49fe3ff7c630
mdufour <mathieu.dufour@whereyoulove.com>
2019-09-27 16:11:19 +0200

    try heroku

---
+++ server/deps/bureaucrat/lib/bureaucrat.ex
@@ -0,0 +1,26 @@
+defmodule Bureaucrat do
+  use Application
+
+  def start(_type, []) do
+    import Supervisor.Spec
+
+    children = [
+      worker(Bureaucrat.Recorder, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: Bureaucrat.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def start(options \\ []) do
+    Application.start(:bureaucrat)
+    configure(options)
+    :ok
+  end
+
+  defp configure(options) do
+    Enum.each(options, fn {k, v} ->
+      Application.put_env(:bureaucrat, k, v)
+    end)
+  end
+end
