fdf9428947d7242aad10b8d5d22572aed573c5bf
mdufour <mathieu.dufour@whereyoulove.com>
2019-09-29 13:50:35 +0200

    change path to gcloud

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 # Make sure we have gcloud installed in travis env
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
-  rm -rf $HOME/google-cloud-sdk;
+if [ ! -d "/home/travis/build/lucas-deangelis/mini-projet01-epitech/google-cloud-sdk/bin" ]; then
+  rm -rf /home/travis/build/lucas-deangelis/mini-projet01-epitech/google-cloud-sdk;
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
-  # curl https://sdk.cloud.google.com | bash > /dev/null; 
-  curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-245.0.0-linux-x86_64.tar.gz;
-  tar zxvf google-cloud-sdk-245.0.0-linux-x86_64.tar.gz google-cloud-sdk;
-  ./google-cloud-sdk/install.sh;
+  curl https://sdk.cloud.google.com | bash > /dev/null; 
 fi
 
 # Promote gcloud to PATH top priority (prevent using old version fromt travis)
-source $HOME/google-cloud-sdk/path.bash.inc
+source /home/travis/build/lucas-deangelis/mini-projet01-epitech/google-cloud-sdk/path.bash.inc
 
 # Make sure kubectl is updated to latest version
 gcloud --quiet components update
