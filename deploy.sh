5977e13cdedf6076e54eb4671ec202473ce6c94c
mdufour <mathieu.dufour@whereyoulove.com>
2019-09-29 12:56:24 +0200

    ...

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 # Make sure we have gcloud installed in travis env
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
-  rm -rf "$HOME/google-cloud-sdk"
-  curl https://sdk.cloud.google.com | bash > /dev/null
-  # curl https://sdk.cloud.google.com
+  rm -rf $HOME/google-cloud-sdk;
+  export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
+  curl https://sdk.cloud.google.com | bash; 
 fi
 
 # Promote gcloud to PATH top priority (prevent using old version fromt travis)
-source $HOME/google-cloud-sdk/path.bash.inc
+source $HOME/travis/google-cloud-sdk/path.bash.inc
 
 # Make sure kubectl is updated to latest version
 gcloud --quiet components update
